a917a9f3c33b04f034b5a98c849378b82e55b59e
Danila Mihailov <danilamihailov@Danilas-MacBook-Pro.local>
2019-09-05 12:00:29 +0500

    fixed typo

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ ln -s .dotfiles/zshrc .zshrc
 ln -s .dotfiles/vimrc .vimrc
 ln -s .dotfiles/tmux.conf .tmux.conf
 
-mkidr -p .config/nvim && cd ./config/nvim
+mkdir -p .config/nvim && cd ./config/nvim
 ln -s ~/.dotfiles/init.vim init.vim
 cd ~
 

3b55af92a536d429bcaaaefb6473cc7869ee694c
Danila Mihailov <danilamihailov@Danilas-MacBook-Pro.local>
2019-09-05 11:24:47 +0500

    updated init script

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 cd ~
 
-rm .zshrc .vimrc rm ~/.config/nvim/init.vim
+rm .tmux.conf .zshrc .vimrc rm ~/.config/nvim/init.vim
 
 ln -s .dotfiles/zshrc .zshrc
 ln -s .dotfiles/vimrc .vimrc
+ln -s .dotfiles/tmux.conf .tmux.conf
 
 mkidr -p .config/nvim && cd ./config/nvim
 ln -s ~/.dotfiles/init.vim init.vim

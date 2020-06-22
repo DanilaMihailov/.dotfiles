12b2a15df72ffb89166c84c9e6629202144d884b
Danila Mihailov <danilamihailov@icloud.com>
2020-06-22 08:36:13 +0500

    add todos

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+;; TODO
+;; make modeline prettier
+;; sessions
+;; configure more lsps
+;; try orgmode
+;; fix slow auto complete
+
 ;; add packages repo
 (require 'package)
 (add-to-list 'package-archives
@@ -31,6 +38,9 @@
 
 ;; No ugly button for checkboxes
 (setq widget-image-enable nil)
+
+;; hide line numbers in mode line
+(line-number-mode nil)
 
 ;; No sound
 (setq visible-bell t)
@@ -85,6 +95,12 @@
 (require 'ido-completing-read+)
 (ido-ubiquitous-mode 1)
 
+(load "~/.emacs.d/evil-unimpared")
+(evil-unimpaired-mode)
+;; does not work :(
+(evil-unimpaired-define-pair "e" '(move-text-up . move-text-down) '(normal visual))
+(evil-unimpaired-define-pair "q" '(flycheck-previous-error . flycheck-next-error))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -95,16 +111,17 @@
  '(custom-enabled-themes (quote (gruvbox-dark-medium)))
  '(custom-safe-themes
    (quote
-    ("aded61687237d1dff6325edb492bde536f40b048eab7246c61d5c6643c696b7f" "e1d09f1b2afc2fed6feb1d672be5ec6ae61f84e058cb757689edb669be926896" default)))
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "aded61687237d1dff6325edb492bde536f40b048eab7246c61d5c6643c696b7f" "e1d09f1b2afc2fed6feb1d672be5ec6ae61f84e058cb757689edb669be926896" default)))
  '(display-line-numbers (quote relative))
  '(git-gutter:added-sign "┃")
  '(git-gutter:deleted-sign "•")
  '(git-gutter:modified-sign "┃")
  '(git-gutter:window-width 1)
  '(global-git-gutter-mode t)
+ '(line-number-mode nil)
  '(package-selected-packages
    (quote
-    (ido-completing-read+ evil-collection git-gutter smooth-scrolling evil-tabs flycheck exec-path-from-shell company lsp-ui rust-mode lsp-mode magit ## evil gruvbox-theme gru which-key beacon)))
+    (rich-minority smart-mode-line ido-completing-read+ evil-collection git-gutter smooth-scrolling evil-tabs flycheck exec-path-from-shell company lsp-ui rust-mode lsp-mode magit ## evil gruvbox-theme gru which-key beacon)))
  '(pdf-view-midnight-colors (quote ("#282828" . "#fbf1c7"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

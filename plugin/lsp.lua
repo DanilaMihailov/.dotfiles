b339e2d3a0dd92846f2e5e5b19fb3449d151e09e
Danila Mihailov <danilamihailov@icloud.com>
2022-07-28 19:12:19 +0500

    fix lua lsp setup

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,4 +1,4 @@
-local servers = {'sumneko_lua', 'rust_analyzer', 'tsserver'} 
+local servers = {'sumneko_lua', 'rust_analyzer', 'tsserver'}
 
 require("mason").setup()
 require("mason-lspconfig").setup({
@@ -46,3 +46,26 @@ for _i, serv in ipairs(servers) do
     on_attach = on_attach
   }
 end
+
+require('lspconfig')["sumneko_lua"].setup {
+    settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}

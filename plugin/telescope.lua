3a517cbbc8ba0158c0884939dcbe82726f2f7d7c
Danila Mihailov <danilamihailov@icloud.com>
2023-04-14 01:18:37 +0500

    add outline binding

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -28,6 +28,7 @@ require("telescope").load_extension "file_browser"
 local telescope = require('telescope.builtin')
 vim.keymap.set("n", "<C-p>", telescope.git_files, { desc = "Search files with telescope" })
 vim.keymap.set("n", "<C-b>", telescope.buffers, { desc = "Search open buffers with telescope" })
+vim.keymap.set("n", "<leader>o", telescope.lsp_document_symbols, { desc = "Show lsp document symbols (outline) with telescope" })
 
 -- vim.keymap.set("n", "<C-f>", function ()
 --     require "telescope".extensions.file_browser.file_browser({grouped = true, select_buffer = true})

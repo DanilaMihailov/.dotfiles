cde3713a23a199fb2497aef695bee1702511520f
Danila Mihailov <danilamihailov@icloud.com>
2024-06-15 21:37:10 +0500

    add apple logo and bracket spaces

--- sketchybar/dot-config/sketchybar/items/calendar.sh
+++ sketchybar/dot-config/sketchybar/items/calendar.sh
@@ -3,4 +3,5 @@
 sketchybar --add item calendar right \
            --set calendar update_freq=30 \
 	   		  icon.font.style=Bold \
+                          padding_right=-5 \
                           script="$CONFIG_DIR/plugins/clock.sh"
--- sketchybar/dot-config/sketchybar/items/spaces.sh
+++ sketchybar/dot-config/sketchybar/items/spaces.sh
@@ -2,10 +2,13 @@
 
 SPACE_SIDS=(1 2 3 4 5 6 7 8 9 10)
 
+
 for sid in "${SPACE_SIDS[@]}"
 do
   sketchybar --add space space.$sid left                                 \
              --set space.$sid space=$sid                                 \
+                              padding_right=0 \
+                              padding_left=0 \
                               icon=$sid                                  \
                               label.font="sketchybar-app-font:Regular:14.0" \
                               label.padding_right=20                     \
@@ -13,6 +16,11 @@ do
                               script="$CONFIG_DIR/plugins/space.sh" \
                               click_script="yabai -m space --focus $sid"
 done
+
+sketchybar --add bracket spaces '/space\..*/'               \
+           --set         spaces background.color=$ITEM_BG_COLOR \
+                                background.corner_radius=4  \
+                                # background.height=20
 
 sketchybar --add item space_separator left                             \
            --set space_separator icon=""                                \

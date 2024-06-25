3aa229fa68ec3267af1a65ec399a92e9e9f211ab
Danila Mihailov <danilamihailov@icloud.com>
2024-06-25 23:45:58 +0500

    update telescope and plugins

--- nvim/dot-config/nvim/lua/custom/plugins/telescope.lua
+++ nvim/dot-config/nvim/lua/custom/plugins/telescope.lua
@@ -53,6 +53,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
       --
       defaults = {
         dynamic_preview_title = true,
+        path_display = { 'truncate' },
         mappings = {
           i = {
             -- ['<c-enter>'] = 'to_fuzzy_refine',
@@ -71,6 +72,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     -- Enable Telescope extensions if they are installed
     pcall(require('telescope').load_extension, 'fzf')
     pcall(require('telescope').load_extension, 'ui-select')
+    pcall(require('telescope').load_extension, 'noice')
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
@@ -87,7 +89,12 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    vim.keymap.set(
+      'n',
+      '<leader>s.',
+      builtin.oldfiles,
+      { desc = '[S]earch Recent Files ("." for repeat)' }
+    )
     vim.keymap.set('n', '<leader>st', builtin.treesitter, { desc = '[S]earch [T]reesitter' })
     vim.keymap.set('n', '<leader><leader>gc', builtin.git_branches, { desc = '[G]it [C]heckout' })
     vim.keymap.set('n', '<leader><leader>gf', builtin.git_files, { desc = '[G]it [F]iles' })

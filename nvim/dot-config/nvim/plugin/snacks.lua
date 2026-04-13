cbd57b06b9013934e8d0d3c88e6f674d816928c6
Danila Mihailov <danilamihailov@icloud.com>
2026-04-13 18:35:06 +0500

    make custom ivy layout

--- nvim/dot-config/nvim/plugin/snacks.lua
+++ nvim/dot-config/nvim/plugin/snacks.lua
@@ -9,7 +9,28 @@ local snacks = require 'snacks'
 
 snacks.setup {
   picker = {
-    layout = { position = 'bottom', preset = 'ivy' },
+    layout = { position = 'bottom', preset = 'ivy_custom' },
+    layouts = {
+      ivy_custom = {
+        layout = {
+          box = 'vertical',
+          backdrop = false,
+          row = -1,
+          width = 0,
+          height = 0.4,
+          border = 'top',
+          title = ' {title} {live} {flags}',
+          title_pos = 'left',
+          { win = 'input', height = 1, border = 'none' },
+          {
+            border = 'none',
+            box = 'horizontal',
+            { win = 'list', border = 'none' },
+            { win = 'preview', border = 'none', title = '{preview}', width = 0.6 },
+          },
+        },
+      },
+    },
   },
 }
 
